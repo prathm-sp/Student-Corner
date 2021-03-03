28f0543300aa9cb0301e0377938e2546ef1c24e0
prathmesh <prathmeshkulkarni98882@gmail.com>
2021-03-03 17:28:14 +0530

    done

--- learnAskill/server/helpers/multer.js
+++ learnAskill/server/helpers/multer.js
@@ -1,19 +1,20 @@
 var multer = require("multer");
 //multer.diskStorage() creates a storage space for storing files.
 var storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		 if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
-		cb(null, "uploads/");
-		}else{
-		 cb({message: 'this file is neither a video or image file'}, false)
-		 }
-	},
-	filename: function (req, file, cb) {
-		cb(null, `${Date.now()}.jpg`);
-	}
+  destination: function (req, file, cb) {
+    console.log("orignal name = ", file);
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+      cb(null, "uploads/");
+    } else {
+      cb({ message: "this file is neither a video or image file" }, false);
+    }
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}.jpg`);
+  },
 });
 var upload = multer({ storage: storage });
-module.exports = { upload }; 
+module.exports = { upload };
 
 // const multer = require('multer')
 // const  Datauri =require('datauri');
