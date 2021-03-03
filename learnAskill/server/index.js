579e30eb9b379b3db2291224a0b6b6becc96c2a8
prathmesh <prathmeshkulkarni98882@gmail.com>
2021-03-03 17:28:20 +0530

    done

--- learnAskill/server/index.js
+++ learnAskill/server/index.js
@@ -3,10 +3,13 @@ const cors = require("cors");
 const logger = require("morgan");
 require("dotenv").config();
 require("./database/mongodbinit");
-
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cors());
-app.use(express.json());
 app.use(logger("dev"));
 
 const allroutes = require("./routes/index");
