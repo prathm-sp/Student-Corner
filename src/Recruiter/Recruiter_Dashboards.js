66ee216df8ea5db983819a33cee405bf6edb4526
prathmesh <prathmeshkulkarni98882@gmail.com>
2021-03-07 11:33:18 +0530

    done

--- src/Recruiter/Recruiter_Dashboards.js
+++ src/Recruiter/Recruiter_Dashboards.js
@@ -1,28 +1,36 @@
-import axios from '../axios'
-import React, { useEffect, useState } from 'react'
-import "./Recruiter_Dashboards.css"
-import { toast } from 'react-toastify'
+import axios from "../axios";
+import React, { useEffect, useState } from "react";
+import "./Recruiter_Dashboards.css";
+import { toast } from "react-toastify";
 toast.configure();
 
-
 function Dashboards() {
-  const token = localStorage.getItem('token')
-  var [data, setDate] = useState()
-  useEffect(() => {
-    if (!token) return null
-    axios.get('/class/student/list', { headers: { Authorization: `Bearer ${token}` } })
-      .then((res) => {
-        console.log(res)
+  const token = localStorage.getItem("token");
+  var [data, setData] = useState();
+  var [reqUrl, setReqUrl] = useState("");
 
-        setDate(res.data.application)
+  useEffect(() => {
+    if (!token) return null;
+    axios
+      .get(`/class/student/list/${reqUrl}`, {
+        headers: { Authorization: `Bearer ${token}` },
       })
-      .catch((err) => { console.log(err.response) })
-  }, [])
+      .then((res) => {
+        console.log(res);
+        setData(res.data.application);
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  }, [reqUrl]);
 
   const handleClick = (val, id) => {
-    console.log(val, id)
+    console.log(val, id);
     if (val == "Accept") {
-      axios.get(`/class/accepted/${id}`, { headers: { Authorization: `Bearer ${token}` } })
+      axios
+        .get(`/class/accepted/${id}`, {
+          headers: { Authorization: `Bearer ${token}` },
+        })
         .then((res) => {
           console.log(res);
           toast.success(`Student Accepted`, {
@@ -30,12 +38,15 @@ function Dashboards() {
             autoClose: false,
           });
           window.location.reload();
-        }).catch((err) => {
-          console.log(err)
         })
-    }
-    else if (val == "Reject") {
-      axios.get(`/class/rejected/${id}`, { headers: { Authorization: `Bearer ${token}` } })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else if (val == "Reject") {
+      axios
+        .get(`/class/rejected/${id}`, {
+          headers: { Authorization: `Bearer ${token}` },
+        })
         .then((res) => {
           console.log(res);
           toast.success(`Student Rejected`, {
@@ -43,11 +54,12 @@ function Dashboards() {
             autoClose: false,
           });
           window.location.reload();
-        }).catch((err) => {
-          console.log(err)
         })
+        .catch((err) => {
+          console.log(err);
+        });
     }
-  }
+  };
   return (
     <div>
       <div className="pcoded-content mt-100">
@@ -58,47 +70,69 @@ function Dashboards() {
                 <div className="row">
                   {/* card1 start */}
                   <div className="col-md-6 col-xl-3">
-                    <div className="card widget-card-1 " style={{ boxShadow: "0 1px 11px 0 rgba(0, 0, 0, 0.12)" }}>
+                    <div
+                      className="card widget-card-1 "
+                      style={{ boxShadow: "0 1px 11px 0 rgba(0, 0, 0, 0.12)" }}
+                    >
                       <div className="card-block-small">
                         <i className="fa fa-list bg-c-blue card1-icon" />
                         <span className="text-c-blue f-w-600">All</span>
                         <h4>49</h4>
                         <div>
                           <span className="f-left m-t-10 text-muted">
-                            <i className="text-c-blue f-16 icofont icofont-tag m-r-10" />All
-                                    </span>
+                            <i className="text-c-blue f-16 icofont icofont-tag m-r-10" />
+                            All
+                          </span>
                         </div>
                       </div>
                     </div>
                   </div>
                   {/* card1 end */}
                   {/* card1 start */}
-                  <div className="col-md-6 col-xl-3">
-                    <div className="card widget-card-1" style={{ boxShadow: "0 1px 11px 0 rgba(0, 0, 0, 0.12)" }}>
+                  <div
+                    className="col-md-6 col-xl-3"
+                    onClick={() => {
+                      setReqUrl("rejected");
+                    }}
+                  >
+                    <div
+                      className="card widget-card-1"
+                      style={{ boxShadow: "0 1px 11px 0 rgba(0, 0, 0, 0.12)" }}
+                    >
                       <div className="card-block-small">
                         <i className="fa fa-window-close bg-c-pink card1-icon" />
                         <span className="text-c-pink f-w-600">Reject</span>
                         <h4>23</h4>
                         <div>
                           <span className="f-left m-t-10 text-muted">
-                            <i className="text-c-pink f-16 icofont icofont-tag m-r-10" />All
-                                    </span>
+                            <i className="text-c-pink f-16 icofont icofont-tag m-r-10" />
+                            All
+                          </span>
                         </div>
                       </div>
                     </div>
                   </div>
                   {/* card1 end */}
                   {/* card1 start */}
-                  <div className="col-md-6 col-xl-3">
-                    <div className="card widget-card-1" style={{ boxShadow: "0 1px 11px 0 rgba(0, 0, 0, 0.12)" }}>
+                  <div
+                    className="col-md-6 col-xl-3"
+                    onClick={() => {
+                      setReqUrl("accepted");
+                    }}
+                  >
+                    <div
+                      className="card widget-card-1"
+                      style={{ boxShadow: "0 1px 11px 0 rgba(0, 0, 0, 0.12)" }}
+                    >
                       <div className="card-block-small">
                         <i className=" fa fa-check bg-c-green card1-icon" />
                         <span className=" text-c-green f-w-600">Accept</span>
                         <h4>45</h4>
                         <div>
                           <span className="f-left m-t-10 text-muted">
-                            <i className="text-c-green f-16 icofont icofont-tag m-r-10" />All
-                                    </span>
+                            <i className="text-c-green f-16 icofont icofont-tag m-r-10" />
+                            All
+                          </span>
                         </div>
                       </div>
                     </div>
@@ -106,15 +140,19 @@ function Dashboards() {
                   {/* card1 end */}
                   {/* card1 start */}
                   <div className="col-md-6 col-xl-3">
-                    <div className="card widget-card-1" style={{ boxShadow: "0 1px 11px 0 rgba(0, 0, 0, 0.12)" }}>
+                    <div
+                      className="card widget-card-1"
+                      style={{ boxShadow: "0 1px 11px 0 rgba(0, 0, 0, 0.12)" }}
+                    >
                       <div className="card-block-small">
                         <i className="fas fa-sync  bg-c-yellow card1-icon" />
                         <span className="text-c-yellow f-w-600">Process</span>
                         <h4>56</h4>
                         <div>
                           <span className="f-left m-t-10 text-muted">
-                            <i className="text-c-yellow f-16 icofont icofont-refresh m-r-10" />Just update
-                                    </span>
+                            <i className="text-c-yellow f-16 icofont icofont-refresh m-r-10" />
+                            Just update
+                          </span>
                         </div>
                       </div>
                     </div>
@@ -122,26 +160,47 @@ function Dashboards() {
                   {/* card1 end */}
 
                   <div className="col-md-12 col-xl-12 ">
-                    <div className="card project-task " style={{ boxShadow: "0 1px 11px 0 rgba(0, 0, 0, 0.12)" }}>
+                    <div
+                      className="card project-task "
+                      style={{ boxShadow: "0 1px 11px 0 rgba(0, 0, 0, 0.12)" }}
+                    >
                       <div className="col-xl-12 col-md-12">
                         <div className="card Recent-Users">
-                          <div className="card-header" style={{ display: "flex", justifyContent: "space-between" }}>
-                            <h5
-                            > Users</h5>
-                            <h5 style={{
-                              position: 'relative',
-                              left: "-17vw"
-                            }}> User Email</h5>
-                            <h5 style={{
-                              position: 'relative',
-                              left: "-21vw"
-                            }}> User Mobile No.</h5>
+                          <div
+                            className="card-header"
+                            style={{
+                              display: "flex",
+                              justifyContent: "space-between",
+                            }}
+                          >
+                            <h5> Users</h5>
                             <h5
                               style={{
-                                position: 'relative',
-                                left: "-36vw"
-                              }}> Class Name</h5>
-
+                                position: "relative",
+                                left: "-17vw",
+                              }}
+                            >
+                              {" "}
+                              User Email
+                            </h5>
+                            <h5
+                              style={{
+                                position: "relative",
+                                left: "-21vw",
+                              }}
+                            >
+                              {" "}
+                              User Mobile No.
+                            </h5>
+                            <h5
+                              style={{
+                                position: "relative",
+                                left: "-36vw",
+                              }}
+                            >
+                              {" "}
+                              Class Name
+                            </h5>
                           </div>
                           <div className="card-block px-0 py-3">
                             <div className="table-responsive">
@@ -149,24 +208,71 @@ function Dashboards() {
                                 <tbody>
                                   {data?.map?.((item) => (
                                     <tr className="unread">
-                                      <td><i class="fa fa-user-circle " style={{ fontSize: "40px" }} aria-hidden="true"></i></td>
                                       <td>
-                                        <h4 className="mb-1">{item?.applicantid?.email}</h4>
-
+                                        <i
+                                          class="fa fa-user-circle "
+                                          style={{ fontSize: "40px" }}
+                                          aria-hidden="true"
+                                        ></i>
                                       </td>
                                       <td>
-                                        <h6 className="text-muted"><i className="fas fa-circle text-c-green f-10 m-r-15" />{item?.applicantid?.mobile}</h6>
+                                        <h4 className="mb-1">
+                                          {item?.applicantid?.email}
+                                        </h4>
                                       </td>
                                       <td>
-                                        <h4 className="mb-1"><i className="fas fa-circle text-c-green f-10 m-r-15" />{item?.classid?.classname}</h4>
-
+                                        <h6 className="text-muted">
+                                          <i className="fas fa-circle text-c-green f-10 m-r-15" />
+                                          {item?.applicantid?.mobile}
+                                        </h6>
                                       </td>
-                                      <td> <input onClick={() => { handleClick('Accept', item?._id) }} type="submit" value="Accpet" className="btn re btn-primary " style={{ width: "20vh", height: "6vh", marginLeft: "25%", marginTop: "0px", fontSize: "15px", backgroundColor: "#8eb553", borderRadius: "12px" }} defaultValue="" />
-                                        <input onClick={() => { handleClick('Reject', item?._id) }} type="submit" value="Reject" className="btn re btn-primary " style={{ width: "20vh", marginLeft: "9%", height: "6vh", marginTop: "0px", fontSize: "15px", backgroundColor: "#da2461", borderRadius: "12px" }} defaultValue="" />
+                                      <td>
+                                        <h4 className="mb-1">
+                                          <i className="fas fa-circle text-c-green f-10 m-r-15" />
+                                          {item?.classid?.classname}
+                                        </h4>
+                                      </td>
+                                      <td>
+                                        {" "}
+                                        <input
+                                          onClick={() => {
+                                            handleClick("Accept", item?._id);
+                                          }}
+                                          type="submit"
+                                          value="Accpet"
+                                          className="btn re btn-primary "
+                                          style={{
+                                            width: "20vh",
+                                            height: "6vh",
+                                            marginLeft: "25%",
+                                            marginTop: "0px",
+                                            fontSize: "15px",
+                                            backgroundColor: "#8eb553",
+                                            borderRadius: "12px",
+                                          }}
+                                          defaultValue=""
+                                        />
+                                        <input
+                                          onClick={() => {
+                                            handleClick("Reject", item?._id);
+                                          }}
+                                          type="submit"
+                                          value="Reject"
+                                          className="btn re btn-primary "
+                                          style={{
+                                            width: "20vh",
+                                            marginLeft: "9%",
+                                            height: "6vh",
+                                            marginTop: "0px",
+                                            fontSize: "15px",
+                                            backgroundColor: "#da2461",
+                                            borderRadius: "12px",
+                                          }}
+                                          defaultValue=""
+                                        />
                                       </td>
                                     </tr>
                                   ))}
-
                                 </tbody>
                               </table>
                             </div>
@@ -178,12 +284,11 @@ function Dashboards() {
                 </div>
               </div>
             </div>
-
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Dashboards
+export default Dashboards;
